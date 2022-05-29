391e73e877fa7b27856bbfa93458c79c5b65cd35
dagbouj hatem <dagboujhatem@gmail.com>
2022-05-29 14:19:18 +0100

    formaat document ts

--- src/app/components/tree-view/tree-view.component.ts
+++ src/app/components/tree-view/tree-view.component.ts
@@ -10,9 +10,9 @@ import { TreeViewService } from 'src/app/services/tree-view.service';
 })
 export class TreeViewComponent implements OnInit {
   nodeData: any[] = [];
-  searchQuery: any="";
+  searchQuery: any = "";
   loading = false;
-  hoverIndex:any = -1;
+  hoverIndex: any = -1;
   nodeGroup = "nodes";
   subNodeGroup = "subNodes";
 
@@ -25,44 +25,44 @@ export class TreeViewComponent implements OnInit {
     this.dragulaService.createGroup(this.nodeGroup, {
       direction: "horizontal",
       removeOnSpill: true,
-      moves: (el, source, target:any) => {
+      moves: (el, source, target: any) => {
         return target.classList.contains("card-header");
       },
     });
   }
 
-  loadAllNodes(){
+  loadAllNodes() {
     this.loading = true;
     this.treeViewService.getAllNodes()
-    .subscribe((response:any)=>{
-      this.loading = false;
-      this.nodeData = response?.result;
-    }, (error:any)=>{
-      console.log(error);
-      this.loading = false;
-    });
+      .subscribe((response: any) => {
+        this.loading = false;
+        this.nodeData = response?.result;
+      }, (error: any) => {
+        console.log(error);
+        this.loading = false;
+      });
   }
 
-  deleteNodeById(id:any){
+  deleteNodeById(id: any) {
     this.treeViewService.deleteConfirmation().then((result) => {
       if (result.value) {
         this.treeViewService.deleteNodeByID(id)
-        .subscribe((response:any)=>{
-          this.toasterService.success('Node has been deleted successfully.', '');
-          this.loadAllNodes();
-        }, (error:any)=>{
-          console.log(error);
-        });
+          .subscribe((response: any) => {
+            this.toasterService.success('Node has been deleted successfully.', '');
+            this.loadAllNodes();
+          }, (error: any) => {
+            console.log(error);
+          });
       }
     })
   }
 
-  public enter(i:any) {
+  public enter(i: any) {
     this.hoverIndex = i;
-   }
+  }
 
-   public leave(i:any) {
+  public leave(i: any) {
     this.hoverIndex = null;
-   }
+  }
 
 }
