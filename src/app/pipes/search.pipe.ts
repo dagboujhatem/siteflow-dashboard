ab4a3545984583f5fadacc75ad2b089abd5b11f7
dagbouj hatem <dagboujhatem@gmail.com>
2022-05-29 21:31:20 +0100

    fix pipe

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -6,15 +6,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(nodeList?: any[], searchQuery?: string): any[] {
-   if(nodeList){
-    if(searchQuery){
-      return nodeList.filter((item:any)=> item?.name?.toLowerCase().includes(searchQuery?.toLowerCase()));
-    }else {
-     return nodeList;
+    if (nodeList) {
+      if (searchQuery) {
+        const searchTerm = searchQuery?.toLowerCase();
+        return nodeList.filter((item: any) => {
+          return this.lookForNestedObject(item, searchTerm);
+        });
+      } else {
+        return nodeList;
+      }
+    } else {
+      return [];
     }
-   }else {
-    return [];
-   }
+  }
+
+  lookForNestedObject(item: any, searchTerm: any): boolean {
+    return (
+      item?.name?.toLowerCase().includes(searchTerm) ||
+      item?.children.some((item: any) => item?.name?.toLowerCase().includes(searchTerm))
+    );
   }
 
 }
